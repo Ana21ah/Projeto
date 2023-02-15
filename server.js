ffc951cdaa872223007a5b8af58e6e8d455d7276
Ana Clara <113737092+Ana21ah@users.noreply.github.com>
2023-02-15 20:52:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
-import jsonServer from 'json-server';
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200;
-server.use(middlewares);
-server.use(router);
-server.listen(port);
+const express = require("express");
+const cors = require('cors')
+const app = express();
+const PORT = 3000;
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+    //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+    //Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
+app.get("/", (requisicao, resposta) => {
+    resposta.json({ message: 'ok' });
+});
+
+
+server.listen(PORT, () => {
+    console.log(`Aplicação rodando em http://localhost:${PORT}`)
+})
